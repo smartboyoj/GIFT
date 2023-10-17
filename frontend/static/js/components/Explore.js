db8a50b4b7578ed09bdf8fa00cbe5a4d585f589d
Smart Boy <smartboyoj432@gmail.com>
2023-10-17 23:28:24 +0530

    Home Page Skeleton Ready

---
+++ frontend/static/js/components/Explore.js
@@ -0,0 +1,25 @@
+export default class {
+    constructor() {
+        document.title = "Explore - GIFTS"
+    }
+
+    async getHtml() {
+        return `<div class="news_section">
+        <div class="newitem n1">
+            <div class="author_logo_section">
+                <img src="https://imgs.search.brave.com/s03cET3RnJt5LHUeHtYq_RyeqThRw963iJTmg5ho1Q4/rs:fit:860:0:0/g:ce/aHR0cHM6Ly93d3cu/YnlyZGllLmNvbS90/aG1iL1BlLWR2dXNT/OFc5dFJDbEQtRDZy/b1BPM2p0VT0vMTUw/MHgwL2ZpbHRlcnM6/bm9fdXBzY2FsZSgp/Om1heF9ieXRlcygx/NTAwMDApOnN0cmlw/X2ljYygpL2plbm5p/ZmVyLWdhcm5lci1o/YWlyLTU2YTA4Nzgy/M2RmNzhjYWZkYWEy/NzZlOC5qcGc" alt="" class="author_logo">
+            </div>
+        </div>
+        <div class="newitem n2">
+            <div class="author_logo_section">
+                <img src="https://imgs.search.brave.com/s03cET3RnJt5LHUeHtYq_RyeqThRw963iJTmg5ho1Q4/rs:fit:860:0:0/g:ce/aHR0cHM6Ly93d3cu/YnlyZGllLmNvbS90/aG1iL1BlLWR2dXNT/OFc5dFJDbEQtRDZy/b1BPM2p0VT0vMTUw/MHgwL2ZpbHRlcnM6/bm9fdXBzY2FsZSgp/Om1heF9ieXRlcygx/NTAwMDApOnN0cmlw/X2ljYygpL2plbm5p/ZmVyLWdhcm5lci1o/YWlyLTU2YTA4Nzgy/M2RmNzhjYWZkYWEy/NzZlOC5qcGc" alt="" class="author_logo">
+            </div>
+        </div>
+        <div class="newitem n3">
+            <div class="author_logo_section">
+                <img src="https://imgs.search.brave.com/s03cET3RnJt5LHUeHtYq_RyeqThRw963iJTmg5ho1Q4/rs:fit:860:0:0/g:ce/aHR0cHM6Ly93d3cu/YnlyZGllLmNvbS90/aG1iL1BlLWR2dXNT/OFc5dFJDbEQtRDZy/b1BPM2p0VT0vMTUw/MHgwL2ZpbHRlcnM6/bm9fdXBzY2FsZSgp/Om1heF9ieXRlcygx/NTAwMDApOnN0cmlw/X2ljYygpL2plbm5p/ZmVyLWdhcm5lci1o/YWlyLTU2YTA4Nzgy/M2RmNzhjYWZkYWEy/NzZlOC5qcGc" alt="" class="author_logo">
+            </div>
+        </div>
+    </div>`;
+    }
+}
--- frontend/static/js/index.js
+++ frontend/static/js/index.js
@@ -1,48 +1,50 @@
-// const navigateTo = url => {
-//     history.pushState(null, null, url);
-//     router();
-// };
+import Explore from "./components/Explore.js"
 
-// const router = async () => {
-//     const routes = [
-//         { path: "/Explore", view: Explore },
-//         { path: "/Authorities", view: Authorities },
-//         { path: "/Communities", view: Communities },
-//         { path: "/Bus Routes", view: Bus_Routes },
-//         { path: "/Profile", view: Profile }
-//     ];
+const navigateTo = url => {
+    history.pushState(null, null, url);
+    router();
+};
 
-//     // Test each route for potential match
-//     const potentialMatches = routes.map(route => {
-//         return {
-//             route: route,
-//             isMatch: location.pathname === route.path
-//         };
-//     });
+const router = async () => {
+    const routes = [
+        { path: "/Explore", view: Explore }
+        // { path: "/Authorities", view: Authorities },
+        // { path: "/Communities", view: Communities },
+        // { path: "/Bus Routes", view: Bus_Routes },
+        // { path: "/Profile", view: Profile }
+    ];
 
-//     let match = potentialMatches.find(potentialMatch => potentialMatch.isMatch);
+    // Test each route for potential match
+    const potentialMatches = routes.map(route => {
+        return {
+            route: route,
+            isMatch: location.pathname === route.path
+        };
+    });
 
-//     if (!match) {
-//         match = {
-//             route: routes[0],
-//             isMatch: true
-//         };
-//     }
+    let match = potentialMatches.find(potentialMatch => potentialMatch.isMatch);
 
-//     const view = new match.route.view;
+    if (!match) {
+        match = {
+            route: routes[0],
+            isMatch: true
+        };
+    }
 
-//     document.querySelector("#app").innerHTML = await view.getHtml();
-// };
+    const view = new match.route.view;
 
-// window.addEventListener("popstate", router);
+    document.querySelector("#app").innerHTML = await view.getHtml();
+};
 
-// document.addEventListener("DOMContentLoaded", () => {
-//     document.body.addEventListener("click", e => {
-//         if (e.target.matches("[data-link]")) {
-//             e.preventDefault();
-//             navigateTo(e.target.href);
-//         }
-//     });
+window.addEventListener("popstate", router);
 
-//     router();
-// });
+document.addEventListener("DOMContentLoaded", () => {
+    document.body.addEventListener("click", e => {
+        if (e.target.matches("[data-link]")) {
+            e.preventDefault();
+            navigateTo(e.target.href);
+        }
+    });
+
+    router();
+});
