893de960f56026d6d1055848d0d8928016b76b7a
Smart Boy <smartboyoj432@gmail.com>
2023-10-17 11:57:02 +0530

    UI Designing Started

---
+++ frontend/static/js/index.js
@@ -0,0 +1,48 @@
+// const navigateTo = url => {
+//     history.pushState(null, null, url);
+//     router();
+// };
+
+// const router = async () => {
+//     const routes = [
+//         { path: "/Explore", view: Explore },
+//         { path: "/Authorities", view: Authorities },
+//         { path: "/Communities", view: Communities },
+//         { path: "/Bus Routes", view: Bus_Routes },
+//         { path: "/Profile", view: Profile }
+//     ];
+
+//     // Test each route for potential match
+//     const potentialMatches = routes.map(route => {
+//         return {
+//             route: route,
+//             isMatch: location.pathname === route.path
+//         };
+//     });
+
+//     let match = potentialMatches.find(potentialMatch => potentialMatch.isMatch);
+
+//     if (!match) {
+//         match = {
+//             route: routes[0],
+//             isMatch: true
+//         };
+//     }
+
+//     const view = new match.route.view;
+
+//     document.querySelector("#app").innerHTML = await view.getHtml();
+// };
+
+// window.addEventListener("popstate", router);
+
+// document.addEventListener("DOMContentLoaded", () => {
+//     document.body.addEventListener("click", e => {
+//         if (e.target.matches("[data-link]")) {
+//             e.preventDefault();
+//             navigateTo(e.target.href);
+//         }
+//     });
+
+//     router();
+// });
